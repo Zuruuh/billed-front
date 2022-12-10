0a8ae57f830977106b8e5968b7efad9f16f4ff00
Zuruuh <ziadi.mail.pro@gmail.com>
2022-12-10 12:07:00 +0100

    🐛 fix a bug where click event listeners would keep on stacking

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -142,22 +142,28 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0;
     if (this.index === undefined || this.index !== index) this.index = index;
+    console.log(this.counter);
+
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' });
+      document.querySelector(`#status-bills-container${this.index}`).setAttribute('data-open', 'true')
       $(`#status-bills-container${this.index}`).html(
         cards(filteredBills(bills, getStatus(this.index)))
       );
       this.counter++;
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' });
+      document.querySelector(`#status-bills-container${this.index}`).setAttribute('data-open', 'false')
       $(`#status-bills-container${this.index}`).html('');
       this.counter++;
     }
+    console.log(this.counter);
 
     bills.forEach((bill) => {
-      $(`#open-bill${bill.id}`).click((e) =>
-        this.handleEditTicket(e, bill, bills)
-      );
+      $(`#status-bills-container${this.index}[data-open=true] #open-bill${bill.id}`).click((e) => {
+        console.log(bill.id);
+        this.handleEditTicket(e, bill, bills);
+      });
     });
 
     return bills;
