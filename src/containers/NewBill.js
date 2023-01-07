d097f9045353f2c268fc14d125305f609e8e039c
Zuruuh <ziadi.mail.pro@gmail.com>
2023-01-07 16:33:57 +0100

    :bug: Fixed bug preventing jpeg images from being submitted.

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -2,7 +2,7 @@ import { ROUTES_PATH } from '../constants/routes.js';
 import Logout from './Logout.js';
 
 export default class NewBill {
-  static IMAGE_ALLOWED_MIME_TYPES = ['image/png', 'image.jpeg'];
+  static IMAGE_ALLOWED_MIME_TYPES = ['image/png', 'image/jpeg'];
 
   constructor({ document, onNavigate, store, localStorage }) {
     this.document = document;
